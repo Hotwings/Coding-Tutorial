d33356c2533c1ee92544231b763afb9f5e76a473
Hotwings <edcorgiat@gmail.com>
2022-04-11 16:11:07 -0500

    Init Commit

---
+++ Coding Tutorial/CodingTutorial/Program.cs
@@ -0,0 +1,16 @@
+﻿using CodingTutorialShared;
+
+using System;
+
+namespace CodingTutorial
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
---
+++ Coding Tutorial/CodingTutorialShared/Game1.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using MonoGame.Extended;
+using MonoGame.Extended.ViewportAdapters;
+
+namespace CodingTutorialShared
+{
+    public class Game1 : Game
+    {
+        public const int SCREEN_WIDTH = 1280;
+        public const int SCREEN_HEIGHT = 720;
+
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D paddle, ball;
+
+        private static OrthographicCamera camera;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = SCREEN_WIDTH;
+            _graphics.PreferredBackBufferHeight = SCREEN_HEIGHT;
+            _graphics.ApplyChanges();
+
+
+            base.Initialize();
+
+            var viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, SCREEN_WIDTH, SCREEN_HEIGHT);
+            camera = new OrthographicCamera(viewportAdapter);
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            paddle = Content.Load<Texture2D>("Images/Paddle");
+            ball = Content.Load<Texture2D>("Images/Ball");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin(transformMatrix: camera.GetViewMatrix());
+
+            _spriteBatch.Draw(paddle, new Vector2(10, 50), Color.White);
+            _spriteBatch.Draw(ball, new Vector2(200, 75), Color.White);
+            _spriteBatch.Draw(paddle, new Vector2(SCREEN_WIDTH - 10, 200), Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
